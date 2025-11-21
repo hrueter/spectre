38544f7ffdb0c22cfd6ae5069d19d4b5d388276d
Hannes Rüter <git@hannesrueter.de>
2025-11-21 20:08:24 +0000

    fixup: Disable xsimd
    for kerr_schild_areal_radius_from_maximal_isotropic root finder

--- src/PointwiseFunctions/AnalyticSolutions/Xcts/Schwarzschild.cpp
+++ src/PointwiseFunctions/AnalyticSolutions/Xcts/Schwarzschild.cpp
@@ -160,7 +160,9 @@ DataVector kerr_schild_areal_radius_from_maximal_isotropic(
     const DataVector& isotropic_radius, const double mass) {
   DataVector lower(isotropic_radius.size(), 1.5 * mass);
   DataVector upper(isotropic_radius.size(), 1e9);
-  return RootFinder::toms748<true>(
+  // Here the version without xsimd is used, because the compiler has problems
+  // matching a function call for "pow"
+  return RootFinder::toms748<false>(
       [&isotropic_radius, &mass](const auto areal_radius, const size_t i) {
         return .25 *
                    (2. * areal_radius + mass +
